f605729107929f21226f9d53e3b05b3bd2994149
brunobarretto22 <87911091+brunobarretto22@users.noreply.github.com>
2025-09-19 12:56:10 -0400

    Update moss_seringueira.py

--- moss_seringueira.py
+++ moss_seringueira.py
@@ -21,8 +21,8 @@ st.markdown(
     unsafe_allow_html=True)
 
 # # Carregar o shapefile dos municípios do Mato Grosso (MT)
-gdf_apd = gpd.read_file('moss_seringueira/PA_APD_Seringueira_Agrupado.shp')
-gdf_aud = gpd.read_file('moss_seringueira/PA_AUD_Seringueira_Agrupado.shp')
+gdf_apd = gpd.read_file('PA_APD_Seringueira_Agrupado.shp')
+gdf_aud = gpd.read_file('PA_AUD_Seringueira_Agrupado.shp')
 
 # print(gdf_apd.head())
 # print(gdf_aud.head())
@@ -58,4 +58,5 @@ folium.GeoJson(
 folium.LayerControl().add_to(m)
 
 st_folium(m, width=None, height=600)
+
 
